8334c6e7394d6729aa38532b41a74954eb01973d
mrhujaifa <mdhujaifaislamshanto@gmail.com>
2025-02-25 22:22:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let loginBtn = document.getElementById("login-Btn");
+
+loginBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let accoutNumber = document.getElementById("account-Number").value;
+
+  let PinNumber = document.getElementById("PIN-number").value;
+
+  let convartedPIN = parseInt(PinNumber);
+
+  let convartedAccountNum = parseInt(accoutNumber);
+
+  if (accoutNumber.length === 11 && convartedAccountNum === 01608215377) {
+    if (convartedPIN === 2343 && PinNumber.length === 4) {
+      window.location.href = "./main.html";
+    } else {
+      alert("Incorrect your PIN number. Please try again later!");
+    }
+  } else {
+    alert("Incorrect your Account Number. Please try again later!");
+  }
+});
