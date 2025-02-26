091c7b6b6af38714ad6d5f63185ee6c30325957b
mrhujaifa <mdhujaifaislamshanto@gmail.com>
2025-02-26 15:11:11 +0600

    update

---
+++ Javascripts/addMoney.js
@@ -0,0 +1,25 @@
+let addMoneybtn = document.getElementById("addMoneyBtn");
+
+addMoneybtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let amount = document.getElementById("Amount").value;
+  let convertedAmount = parseFloat(amount);
+
+  let PIN = document.getElementById("pin").value;
+  let convertedPin = parseInt(PIN);
+
+  let mainBalance = document.getElementById("mainBalance").innerText;
+  let convertedmainBalance = parseFloat(mainBalance);
+
+  if (amount && PIN) {
+    if (convertedPin === 2343) {
+      const totalAmount = convertedmainBalance + convertedAmount;
+      document.getElementById("mainBalance").innerText = totalAmount;
+    } else {
+      alert('invlid pin number ')
+    }
+    
+  } else {
+    alert("Enter your amouont!!!");
+  }
+});
